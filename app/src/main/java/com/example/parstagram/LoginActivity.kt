34ba94511b78d12645d0181c0acfa7ea04a20c48
smahavadi <svmahavadi@gmail.com>
2022-03-25 22:29:24 -0400

    able to log in, not able to do anything after that yet

--- app/src/main/java/com/example/parstagram/LoginActivity.kt
+++ app/src/main/java/com/example/parstagram/LoginActivity.kt
@@ -2,8 +2,11 @@ package com.example.parstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.parse.ParseUser
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +20,17 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun loginUser(username: String, passowrd: Any) {
-
+    private fun loginUser(username: String, password: String) {
+        ParseUser.logInInBackground(username, password, ({ user, e ->
+            if (user != null) {
+                Log.i(TAG, "Successfully logged in user")
+            } else {
+                e.printStackTrace()
+                Toast.makeText(this, "Error logging in", Toast.LENGTH_SHORT).show()
+            }})
+        )
+    }
+    companion object {
+        const val TAG = "LoginActivity"
     }
 }
